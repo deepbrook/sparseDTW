d1d0cd6ec81967ac928a9199580a963cdbdf0d49
nlsdfnbch <23okrs35+github@mykolab.com>
2016-09-23 13:59:11 +0200

    updated imports in tests accordingly

--- tests.py
+++ tests.py
@@ -10,29 +10,29 @@ from unittest import TestCase
 import numpy as np
 # Import Homebrew
 
-import dtw
+import sparse_dtw
 # Init Logging Facilities
 log = logging.getLogger(__name__)
 
 
-class SparseDTWTest(TestCase):
+class Sparse_dtwTest(TestCase):
 
     def setUp(self):
         self.s = [3, 4, 5, 3, 3]
         self.q = [1, 2, 2, 1, 0]
 
     def test_quantize(self):
-        a, b = dtw.quantize(self.s, self.q)
+        a, b = sparse_dtw.quantize(self.s, self.q)
         check_a = np.array([0.0, 0.5, 1.0, 0.0, 0.0])
         check_b = np.array([0.5, 1.0, 1.0, 0.5, 0.0])
         self.assertTrue(np.array_equal(a, check_a))
         self.assertTrue(np.array_equal(b, check_b))
 
     def test_euc_dist(self):
-        self.assertEqual(dtw.euc_distance(5,7), 4)
+        self.assertEqual(sparse_dtw.euc_distance(5,7), 4)
 
     def test_populate_warp(self):
-        a = dtw.populate_warp(self.s, self.q, 0.5)
+        a = sparse_dtw.populate_warp(self.s, self.q, 0.5)
         b = np.array([[4, 0, 0, 4, 9],
                       [9, 4, 4, 9, 16],
                       [16, 9, 9, 16, 0],
@@ -41,8 +41,8 @@ class SparseDTWTest(TestCase):
         self.assertTrue(np.array_equal(a, b))
 
     def test_unblock_warp_matrix(self):
-        SM = dtw.populate_warp(self.s, self.q, 0.5)
-        a = dtw.calculat_warp_costs(self.s, self.q, SM)
+        SM = sparse_dtw.populate_warp(self.s, self.q, 0.5)
+        a = sparse_dtw.calculat_warp_costs(self.s, self.q, SM)
         b = np.array([[4, 0, 0, 4, 13],
                       [13, 8, 12, 13, 20],
                       [29, 17, 17, 28, 38],
@@ -50,8 +50,8 @@ class SparseDTWTest(TestCase):
                       [37, 34, 35, 25, 30]])
         self.assertTrue(np.array_equal(a, b))
 
-    def test_sparseDTW(self):
-        sparsed = dtw.sparse_dtw(self.s, self.q, res=0.5)
+    def test_sparsesparse_dtw(self):
+        sparsed = sparse_dtw.sparse_dtw(self.s, self.q, res=0.5)
         check = [(4, 4), (3, 3), (2, 2), (1, 1), (0, 0)]
         check.reverse()
         self.assertTrue(sparsed == (check, 30))
